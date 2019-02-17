c85cce2191394e499451b4b439a81f64c12aa449
abinabraham <abin.abraham@in.theory-y.com>
2019-02-18 02:27:12 +0530

    make it better

--- belemaoil/urls.py
+++ belemaoil/urls.py
@@ -39,7 +39,7 @@ urlpatterns = [
 
     url(r'^logout/$', auth_views.logout, {'next_page': '/login'}, name='logout'), 
 
-]++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
